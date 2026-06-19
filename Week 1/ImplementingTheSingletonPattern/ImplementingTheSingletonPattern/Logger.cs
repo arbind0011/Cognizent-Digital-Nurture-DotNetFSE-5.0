1d707cf70d738685d3a57312ad3cd9698615323b
Arbind <arbind9632@gmail.com>
2026-06-19 11:28:13 +0530

    singletonPattern

---
+++ Week 1/ImplementingTheSingletonPattern/ImplementingTheSingletonPattern/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementingTheSingletonPattern
+{
+    public class Logger
+    {
+        private static readonly Logger _instance = new Logger();
+
+        private Logger()
+        {
+            Console.WriteLine("Logger initialized.");
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG]: {message}");
+        }
+    }
+}
---
+++ Week 1/ImplementingTheSingletonPattern/ImplementingTheSingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ImplementingTheSingletonPattern;
+
+Logger logger1 = Logger.Instance;
+logger1.Log("This is the first log message.");
+
+Logger logger2 = Logger.Instance;
+logger2.Log("This is the second log message.");
+
+if (Object.ReferenceEquals(logger1, logger2))
+{
+    Console.WriteLine("SUCCESS: Both logger variables contain the same instance.");
+}
+else
+{
+    Console.WriteLine("FAILURE: The instances are different.");
+}
+
+Console.ReadLine();
